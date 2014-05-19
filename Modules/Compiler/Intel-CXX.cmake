da05182d851b59c34a5a993f29567044ebaeccd4
Stephen Kelly <steveire@gmail.com>
2014-05-19 10:45:58 +0200

    Intel features.

--- Modules/Compiler/Intel-CXX.cmake
+++ Modules/Compiler/Intel-CXX.cmake
@@ -10,3 +10,30 @@ set(CMAKE_DEPFILE_FLAGS_CXX "-MMD -MT <OBJECT> -MF <DEPFILE>")
 
 set(CMAKE_CXX_CREATE_PREPROCESSED_SOURCE "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -E <SOURCE> > <PREPROCESSED_SOURCE>")
 set(CMAKE_CXX_CREATE_ASSEMBLY_SOURCE "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -S <SOURCE> -o <ASSEMBLY_SOURCE>")
+
+if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 14.0)
+  return()
+endif()
+
+if (NOT UNIX OR APPLE OR WIN32)
+  return()
+endif()
+
+set(CMAKE_CXX98_STANDARD_COMPILE_OPTION "-std=c++98")
+set(CMAKE_CXX98_EXTENSION_COMPILE_OPTION "-std=gnu++98")
+
+set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++11")
+set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "-std=gnu++11")
+
+set(CMAKE_CXX_STANDARD_DEFAULT 98)
+
+macro(cmake_record_cxx_compile_features)
+  macro(_get_intel_features std_version list)
+    record_compiler_features(CXX "-std=${std_version}" ${list})
+  endmacro()
+
+  _get_intel_features(c++11 CMAKE_CXX11_COMPILE_FEATURES)
+  if (_result EQUAL 0)
+    _get_intel_features(c++98 CMAKE_CXX98_COMPILE_FEATURES)
+  endif()
+endmacro()
